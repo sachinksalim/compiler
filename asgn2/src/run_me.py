2f155ba44452fa17356319b63ebcc32ca8e0f6f5
sughoshpatil <sughoshpatil@gmail.com>
2018-02-06 01:24:41 +0530

    Done with basic block seperation

--- asgn2/src/run_me.py
+++ asgn2/src/run_me.py
@@ -1,46 +1,71 @@
 #!/usr/bin/python
 
-from tables import *
+# some imports
 
 #
 # some definitions
-
-# Operators
-arith_ops = ['+', '-', '*', '/', '%']
-rel_ops = ['<', '<=', '>', '>=', '==', '!=']
-logic_ops = ['!', '&&', '||']
-
-keywords = ['ifgoto', 'goto', 'return', 'call', 'print', 'label', '=', 'function', 'exit']
+# block_leaders stores the line numbers of the block leaders
+temp_block_leaders = []
+block_leaders = []
+basic_blocks = []
+#op = ['=',]
 
 def main():
-    with open("test.txt") as fp:
-        tac = fp.read() # three-address code
-    tac = tac.split('\n')
-
-    for i in range(len(tac)):
-        tac[i] = tac[i].split(", ")
-    print(tac)
-## start segregating the basic blocks  ##
-## first find the block leaders ##
+	fp = open("test.txt")
+	tac = fp.read() # three-address code
+	tac = tac.split('\n')
+	num_lines_tac  = len(tac)
+	for i in range(len(tac)):
+		tac[i] = tac[i].split(", ")
+	#print(tac)
+###############################################################
+#					Block leaders
+################################################################
 # for call=goto, ifgoto, label, ret#
 ## according to wikipedia, definition of a leader:
-#   1)The first instruction is a leader.
-#   2)The target of a conditional or an unconditional goto/jump instruction is a leader.
-#   3)The instruction that immediately follows a conditional goto/jump instruction is a leader.
-    block_leaders = set() # block_leaders stores the line numbers of the block leaders
-    block_leaders.add(1)  # since the first line is always a leader
-    for line in tac:
-        if line[1] == 'call':
-            block_leaders.add(int(line[0])+1)
-        if line[1] == 'ifgoto':
-            block_leaders.add(int(line[5])) # since target of a jump is also a leader
-            block_leaders.add(int(line[0])+1) 
-        elif line[1] == 'label':
-            block_leaders.add(int(line[0]))   
-        elif line[1] == 'ret':
-            block_leaders.add(int(line[0]))
-    block_leaders = list(sorted(block_leaders))
-    print(block_leaders)
+# 	1)The first instruction is a leader.
+#	2)The target of a conditional or an unconditional goto/jump instruction is a leader.
+#	3)The instruction that immediately follows a conditional goto/jump instruction is a leader.
+	temp_block_leaders.append(1)	# since, the first line is always a leader
+	temp = 1;
+	for i in range(num_lines_tac):
+#		if tac[i][1] == 'call':
+#			print tac[i][0]
+#			block_leaders.append(int(tac[i][0])+1)
+		if tac[i][1] == 'ifgoto':
+			#print tac[i][0]
+			temp_block_leaders.append(int(tac[i][5])) # since target of a jump is also a leader
+			temp_block_leaders.append(int(tac[i][0])+1) 
+		elif tac[i][1] == 'label':
+			#print tac[i][0]
+			temp_block_leaders.append(int(tac[i][0]))	
+		if tac[i][1] == 'ret':
+			#print tac[i][0]
+			temp_block_leaders.append(int(tac[i][0]))
+	temp_block_leaders.sort()
+	# lets remove duplicates
+	for i in temp_block_leaders:
+		if i not in block_leaders:
+			block_leaders.append(i)
+	print block_leaders
+############################################################
+# lets now build the basic blocks
+### lets just put a special character '$' just before a block leader
+
+	for i in range(num_lines_tac):
+		basic_blocks.append(tac[i])
+	#print basic_blocks
+	temp=0
+	for i in block_leaders:
+		basic_blocks.insert( i+temp-1, "$")
+		temp = temp+1
+	print basic_blocks
+# we have finished seperating the basic blocks by '$'
+################################################################
+
+	fp.close()
 
 if __name__ == '__main__':
-    main()
+	main()
+
+# block leaders are (1,5,7,8,10)
