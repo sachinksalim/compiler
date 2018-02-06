02c5e35ab78497f0e33a4d2be1ad32624e24b99e
sachinksalim <sachinksalim@gmail.com>
2018-02-06 04:43:26 +0000

    Update run_me.py

--- asgn2/src/run_me.py
+++ asgn2/src/run_me.py
@@ -7,27 +7,20 @@ from tables import *
 
 # Operators
 arith_ops = ['+', '-', '*', '/', '%']
-rel_ops = ['<', '<=', '>', '>=', '==', '!=']
+rel_ops = ['lt', 'leq', 'gt', 'geq', 'eq', 'neq']
 logic_ops = ['!', '&&', '||']
+operators = arith_ops + rel_ops + logic_ops + ['=']
 
-keywords = ['ifgoto', 'goto', 'return', 'call', 'print', 'label', '=', 'function', 'exit']
+keywords = ['ifgoto', 'goto', 'ret', 'call', 'print', 'label', 'function', 'exit']
 
-def main():
-    with open("test.txt") as fp:
-        tac = fp.read() # three-address code
-    tac = tac.split('\n')
+lang = operators + keywords
 
-    for i in range(len(tac)):
-        tac[i] = tac[i].split(", ")
-    # print(tac)
-###############################################################
-#                   Block leaders
-################################################################
-# for call=goto, ifgoto, label, ret#
-## according to wikipedia, definition of a leader:
-#   1)The first instruction is a leader.
-#   2)The target of a conditional or an unconditional goto/jump instruction is a leader.
-#   3)The instruction that immediately follows a conditional goto/jump instruction is a leader.
+# Variables
+basic_blocks = [] # The list of blocks
+
+
+def form_blocks(tac):
+    basic_blocks = []
     block_leaders = set() # block_leaders stores the line numbers of the block leaders
     block_leaders.add(1)  # since the first line is always a leader
     for line in tac:
@@ -42,17 +35,46 @@ def main():
             block_leaders.add(int(line[0]))
     block_leaders = list(sorted(block_leaders))
     print(block_leaders)
-############################################################
-# lets now build the basic blocks
-### lets just put a special character '$' just before a block leader
-    basic_blocks = []
+
+    new_block = []
     for line in tac:
-        if int(line[0]) in block_leaders:
-            basic_blocks.append('$')
-        basic_blocks.append(line)
-    print (basic_blocks)
-# we have finished seperating the basic blocks by '$'
-################################################################
+        new_block.append(line)
+        if (int(line[0])+1) in block_leaders:
+            basic_blocks.append(new_block)
+            new_block = []
+    for block in basic_blocks:    
+        print (block)
+    return basic_blocks
+
+def process(block):
+    return
 
 if __name__ == '__main__':
-    main()
+    with open("test.txt") as fp:
+        tac = fp.read() # three-address code
+    tac = tac.split('\n')
+
+    for i in range(len(tac)):
+        tac[i] = tac[i].split(", ")    
+    
+    basic_blocks = form_blocks(tac)
+
+    for line in tac:
+        for word in line:
+            if word not in (lang + var_list) and line[1] not in ['label', 'call']:
+                try:
+                    int(word)
+                except:
+                    var_list.append(word)
+    print(var_list)
+
+    print ('.section .text')
+    print ('.global _start')
+    print ('_start:')
+
+    inst_no = 1
+    for block in basic_blocks:
+        if block[0][1] != 'label':
+            print ('L' + str(inst_no) + ':')
+        process(block)
+        inst_no += 1
