989a206af98c9439de9646210f930502c8dc5c57
sachinksalim <sachinksalim@gmail.com>
2018-02-10 10:46:09 +0000

    Update tables.py

--- asgn2/src/tables.py
+++ asgn2/src/tables.py
@@ -3,20 +3,20 @@
 # Initially all the registers are empty
 reg_desc = {}
 
-# List of all the registers in X86
-reg_list = ['eax', 'ebx', 'ecx', 'edx', 'esi', 'edi', 'ebp', 'esp']
-
-var_list = [] # List of all variables
+# Tuple of all the registers in X86
+# reg_list = ('eax', 'ebx', 'ecx', 'edx', 'esi', 'edi', 'ebp', 'esp')
+reg_list = ('eax', 'ebx')
+var_set = set() # Set of all variables
 
 for reg in reg_list:
-    reg_desc[reg] = {'state': 'empty', 'content': -1}
+    reg_desc[reg] = {'state': 'empty', 'content': None}
 
 # Address Descriptor
 # Keep track of location where current value of the name can be found at runtime
 # The location might be a register, stack, memory address or a set of those
 addr_desc = {} 
 
-def find_farthest_use():
+def find_farthest_use(symbol_table):
     farthest_use = {'idx': -1, 'var': '', 'reg': 0}
     for reg in reg_list:
         var = reg_desc[reg]['content']
@@ -27,9 +27,9 @@ def find_farthest_use():
             farthest_use['idx'] = max(farthest_use['idx'],symbol_table[var]['next_use'])
     return farthest_use
 
-def get_reg(var):
+def get_reg(var, symbol_table):
     if addr_desc[var]['loc'] == 'reg': # If var is in register
-        return addr_desc[var]['reg_val'] # Return register of var
+        return (True, addr_desc[var]['reg_val']) # Return register of var
 
     for reg in reg_list:
         if reg_desc[reg]['state'] == 'empty':
@@ -37,10 +37,11 @@ def get_reg(var):
             reg_desc[reg]['content'] = var
             addr_desc[var]['loc'] = 'reg'
             addr_desc[var]['reg_val'] = reg
-            return reg # Return empty register of var
+            return (False, reg) # Return empty register of var
 
 
-    farthest_use = find_farthest_use()
+    farthest_use = find_farthest_use(symbol_table)
+    print('\n\n\n\n\n..............')
     reg = farthest_use['reg']
 
     print ("\tmovl %" + str(reg) + ", " + str(farthest_use['var']))
@@ -49,4 +50,4 @@ def get_reg(var):
     addr_desc[var]['reg_val'] = reg
     reg_desc[reg]['state'] = 'loaded'
     reg_desc[reg]['var'] = var
-    return reg
+    return (False, reg)
