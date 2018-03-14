b0cc37717cea08be256c03a0de5fe511c6429998
sachinksalim <sachinksalim@gmail.com>
2018-03-14 17:14:07 +0000

    Update process_log.py

--- asgn3/process_log.py
+++ asgn3/process_log.py
@@ -11,24 +11,20 @@ def process_derivation():
     output.append(deriv_list[0][0])
     write_html.write('<p>Non-terminal being expanded is shown in <b>BOLD</b>. ')
     write_html.write('Derived expansion is shown in <u>UNDERLINE</u>.</p>')
-    write_html.write('<p><b>' + deriv_list[0][0] + '</b></p>')
     new_deriv = (0, 0)
     for deriv in deriv_list:
-        print_out = []
-        
         for i in range(len(output) - 1, -1, -1):
             if output[i] == deriv[0]:
                 break
-
-        output = output[:i] + deriv[2:] + output[i+1:]
         print_out = output[:]
-        new_deriv = (i, i + len(deriv[2:]) - 1)
+        output = output[:i] + deriv[2:] + output[i+1:]
         print_out[i] = '<b>' + print_out[i] + '</b>'
         print_out[new_deriv[0]] = '<u>' + print_out[new_deriv[0]]
         print_out[new_deriv[1]] = print_out[new_deriv[1]] + '</u>'
         write_html.write('<p>')
         write_html.write(" ".join(print_out))
         write_html.write('</p>\n')
+        new_deriv = (i, i + len(deriv[2:]) - 1)
 
 
 if __name__ == '__main__':
