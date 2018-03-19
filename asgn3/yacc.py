54104ec5ab93169cfe615ffb875c8dabc267d52a
shivam2612 <shivamyadav.iitk@gmail.com>
2018-03-19 06:02:19 +0530

    almost all grammar rules have been written and checked

--- asgn3/yacc.py
+++ asgn3/yacc.py
@@ -7,42 +7,49 @@ import sys
 def p_program(p):
     'start : statements'
 
-def p_statements(p):
-    '''statements   : statement statements 
-                    | statement'''
+def p_empty(p):
+    ''' empty :'''
+    pass
+
+def p_statements(p):   # it is used as statement*
+    ''' statements : statement statements 
+                   | empty'''
 
 def p_statement(p):
-    '''statement    : block 
-                    | SemiColon
-                    | variableStatement 
-                    | expressionStatement
-                    | ifStatement
-                    | iterationStatement
-                    | continueStatement
-                    | breakStatement
-                    | returnStatement
-                    | withStatement
-                    | switchStatement
-                    | printStatement'''
-                    # functionDeclaration is needed to look because it contains many deleted rules
+    ''' statement : block 
+                  | SemiColon
+                  | variableStatement 
+                  | expressionStatement
+                  | ifStatement
+                  | iterationStatement
+                  | continueStatement
+                  | breakStatement
+                  | returnStatement
+                  | withStatement
+                  | switchStatement
+                  | printStatement'''
+                  # functionDeclaration is needed to look because it contains many deleted rules
 
 def p_block(p):
-    'block  : LeftBrace statements RightBrace'
+    ''' block : LeftBrace statements RightBrace'''
 
 def p_variableStatement(p):
-    '''variableStatement : var variableDeclarationList SemiColon
-    | variableDeclarationList SemiColon'''
+    ''' variableStatement : var variableDeclarationList SemiColon
+                          | variableDeclarationList SemiColon'''
 
 def p_variableDeclarationList(p):
-    '''variableDeclarationList : variableDeclaration Comma variableDeclarationList 
-    | variableDeclaration'''
+    ''' variableDeclarationList : variableDeclaration Comma variableDeclarationList 
+                                | variableDeclaration'''
 
 def p_variableDeclaration(p):
-    '''variableDeclaration : Identifier 
-    | Identifier Assign singleExpression'''
+    ''' variableDeclaration : Identifier_tmp 
+                            | Identifier_tmp Assign singleExpression
+        Identifier_tmp : Identifier
+                       | arrayLiteral
+                       | objectLiteral'''
 
 def p_expressionStatement(p):
-    'expressionStatement : expressionSequence SemiColon'
+    ''' expressionStatement : expressionSequence SemiColon'''
 
 def p_ifStatement(p):
     ''' ifStatement : if LeftParen expressionSequence RightParen statement
@@ -51,57 +58,203 @@ def p_ifStatement(p):
 def p_iterationStatement(p):
     ''' iterationStatement  : do statement while LeftParen expressionSequence RightParen SemiColon
                             | while LeftParen expressionSequence RightParen statement
+                            | for LeftParen expressionSequence_r01 SemiColon expressionSequence_r01 SemiColon expressionSequence_r01 RightParen statement
+                            | for LeftParen var variableDeclarationList SemiColon expressionSequence_r01 SemiColon expressionSequence_r01 RightParen statement
                             | for LeftParen singleExpression in expressionSequence RightParen statement
-                            | for LeftParen var variableDeclaration in expressionSequence RightParen statement'''
-                         # doubt here   
-                         #   | for LeftParen expressionSequence? SemiColon expressionsequence? SemiColon expressionsequence? RightParen statement
-                         #   | for LeftParen var variabledeclarationlist SemiColon expressionsequence? SemiColon expressionsequence? RightParen statement
+                            | for LeftParen var variableDeclaration in expressionSequence RightParen statement
+                            
+        expressionSequence_r01 : expressionSequence
+                               | empty'''
 
 def p_continueStatement(p):
-    '''continueStatement : continue Identifier SemiColon
-                         | continue SemiColon'''
+    ''' continueStatement : continue Identifier SemiColon
+                          | continue SemiColon'''
 
 def p_breakStatement(p):
-    '''breakStatement : break Identifier SemiColon
-                      | break SemiColon'''
+    ''' breakStatement : break Identifier SemiColon
+                       | break SemiColon'''
 
 def p_returnStatement(p):
-    '''returnStatement : return expressionSequence SemiColon
-                       | return SemiColon'''
+    ''' returnStatement : return expressionSequence SemiColon
+                        | return SemiColon'''
 
 def p_withstatement(p):
-    '''withStatement : with LeftParen expressionSequence RightParen statement'''
+    ''' withStatement : with LeftParen expressionSequence RightParen statement'''
 
 def p_switchStatement(p):
-    '''switchStatement : switch LeftParen expressionSequence RightParen caseBlock'''
+    ''' switchStatement : switch LeftParen expressionSequence RightParen caseBlock'''
 
 def p_caseBlock(p):
-    ''' caseBlock : LeftBrace caseClauses defaultClause caseClauses RightBrace
-                  | LeftBrace caseClauses defaultClause RightBrace
-                  | LeftBrace caseClauses RightBrace
-                  | LeftBrace defaultClause caseClauses RightBrace
-                  | LeftBrace defaultClause RightBrace
-                  | LeftBrace RightBrace'''
+    ''' caseBlock       : LeftBrace caseClauses_r01 defaultClause_caseClauses_r01 RightBrace
+        caseClauses_r01 : caseClauses
+                        | empty
+        defaultClause_caseClauses_r01 : defaultClause caseClauses_r01
+                        | empty'''
+                
 
 def p_caseClauses(p):
     ''' caseClauses : caseClause caseClauses 
                     | caseClause'''
 
 def p_caseClause(p):
-    ''' caseClause : case expressionSequence Colon statements 
-                   | case expressionSequence Colon'''
+    ''' caseClause : case expressionSequence Colon statements'''
 
-def p_defaultClause(p): # default is be added to keywords in lex.py
-    '''defaultClause : default Colon statements
-                     | default Colon'''
+def p_defaultClause(p):
+    ''' defaultClause : default Colon statements'''
 
-##############################################################
-def p_expressionSequence(p):
-    '''expressionSequence : singleExpression Comma expressionSequence 
-    | singleExpression'''
+## def p_functionDeclaration(p): remaining
 
+## def p_functionBody(p): remaining
+
+def p_arrayLiteral(p):
+    ''' arrayLiteral : LeftBracket comma_r elementList_r01 comma_r RightBracket
+        elementList_r01 : elementList
+                        | empty
+        comma_r : Comma comma_r 
+                | empty'''    
+
+# some doubts in ','+ in grammar of assignment 0 for elementList and arguments
+def p_elementList(p):
+    ''' elementList : singleExpression comma_r1_singleExpression_r
+        comma_r1_singleExpression_r : comma_r1 singleExpression comma_r1_singleExpression_r
+                                    | empty
+        comma_r1 : Comma comma_r1
+                 | Comma '''
+
+def p_arguments(p):
+    ''' arguments : LeftParen expressionSequence RightParen
+                  | LeftParen RightParen'''
+
+def p_expressionSequence(p):    
+    ''' expressionSequence : singleExpression Comma expressionSequence 
+                           | singleExpression'''
+
+# doubt in singleExpression first production involving function
 def p_singleExpression(p):
-    'singleExpression : expression'
+    ''' arguments_r01 : arguments
+                      | empty
+        unaryOp : Incr
+                | Decr
+                | Plus
+                | Minus
+                | BinNot
+                | Not
+        binaryOpAirth : Plus
+                      | Minus
+                      | Times
+                      | Divide
+                      | Mod
+        shiftOp : Lshift
+                | Rshift
+                | Urshift
+        logicalOpComp : LT
+                       | GT
+                       | LTE
+                       | GTE
+        logicalOpEqual : Equal
+                        | NotEqual
+                        | StrEqual
+                        | StrNotEqual
+        binaryOpBit : BinAnd
+                    | BinXor
+                    | BinOr
+                    | And
+                    | Or
+
+        singleExpression : singleExpression LeftBracket expressionSequence RightBracket
+                         | singleExpression Dot identifierName
+                         | singleExpression arguments
+                         
+                         | new singleExpression arguments_r01
+                         | singleExpression Incr
+                         | singleExpression Decr
+                         | delete singleExpression
+                         | void singleExpression
+                         
+                         | unaryOp singleExpression
+                         | singleExpression binaryOpAirth singleExpression
+                         | singleExpression shiftOp singleExpression
+                         | singleExpression logicalOpComp singleExpression
+                         | singleExpression in singleExpression
+                         | singleExpression logicalOpEqual singleExpression
+                         | singleExpression binaryOpBit singleExpression
+                         | singleExpression CondOp singleExpression Colon singleExpression
+                         | singleExpression Assign singleExpression
+                         | singleExpression assignmentOperator singleExpression
+                         | this
+                         | undefined
+                         | Identifier
+                         | literal
+                         | arrayLiteral
+                         | objectLiteral
+                         | LeftParen expressionSequence RightParen'''
+                         #| eval LeftParen program  RightParen
+                         #| Typeof singleExpression
+
+def p_assignmentOperator(p):
+    ''' assignmentOperator : IntoEq
+                           | DivEq
+                           | ModEq
+                           | PlusEq
+                           | MinusEq
+                           | LshiftEq
+                           | RshiftEq
+                           | UrshiftEq
+                           | AndEq
+                           | XorEq
+                           | OrEq'''
+
+def p_literal(p):
+    '''literal : NullLiteral
+               | BooleanLiteral
+               | StringLiteral
+               | DecimalLiteral'''
+               #| TemplateStringLiteral
+               
+def p_objectLiteral(p):
+    ''' objectLiteral : LeftBrace propertyAssignment comma_propertyAssignment_r comma_r01 RightBrace
+                      | LeftBrace comma_r01 RightBrace
+        comma_propertyAssignment_r : Comma propertyAssignment comma_propertyAssignment_r
+                                   | empty
+        comma_r01 : Comma
+                  | empty'''
+
+def p_propertyAssignment(p):
+    '''propertyAssignment : propertyName Colon singleExpression
+                          | propertyName Equal singleExpression
+                          | LeftBracket singleExpression RightBracket Colon singleExpression
+                          | Identifier'''
+
+def p_propertyName(p):
+    '''propertyName : identifierName
+                    | StringLiteral
+                    | DecimalLiteral'''
+
+def p_identifierName(p):
+    ''' identifierName : Identifier
+                       | reservedWord'''
+
+def p_reservedWord(p):
+    '''reservedWord : NullLiteral
+                    | BooleanLiteral'''
+                    #| keywords'''  # how to include keywords of lex.py
+                    
+# identifier already deined in lex.py#
+
+def p_StringLiteral(p): # string is defined in lex.py
+    ''' StringLiteral : String'''
+
+## def p_TemplateStringLiteral(p): remaining    
+
+def p_BooleanLiteral(p):
+    ''' BooleanLiteral : true
+                       | false'''
+
+def p_NullLiteral(p):
+    ''' NullLiteral : null'''
+
+def p_DecimalLiteral(p): # Number is defined in lex.py
+    ''' DecimalLiteral : Number'''
 
 def p_printStatement(p):
     'printStatement : console Dot log LeftParen factor RightParen'
